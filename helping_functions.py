8fe0f0db9c4ac6d981efd3b7d7c70eaa580fc83a
Kevin F <knightkevin99@gmail.com>
2023-01-10 12:46:59 -0600

    Se agregó el manejo de error al momento de cerrar la ventana y el programa seguia en ejecucción

--- helping_functions.py
+++ helping_functions.py
@@ -1,9 +1,10 @@
 infinito = 1000000000
 
-# Con la ayuda de la siguiente funcion creamos la lista de adyacencia de nuestro grafo
-# Los parametros es la matrix de adyacencia que se pidio al usuario
-# Para la lista de adyacencia se esta ocupando una tabla hash, en python se ocupa un diccionario
-def crear_adjList(entrada):
+# Con la ayuda de la siguiente funcion limpiamos la entrada del usuario para la construcción del grafo
+# Primero separamos renglon por renglon
+# Luego identificamos el nodo origen en cada renglon y las conexiones que tiene con cada otro nodo
+
+def limpiar_entrada(entrada):
     temp = entrada.split('\n')
     adj = {}
 
@@ -13,11 +14,14 @@ def crear_adjList(entrada):
         b = []
 
         for j in range (0, len(a[1])):
+            # Si no hay camino o algun lazo, usamos una varible la cual no tomaremos en cuenta
             if  a[1][j] == 'x' or a[1][j] == '-' : 
                 b.append(infinito)
             else :
                 b.append(int(a[1][j]))
 
+
+        # Para poder tener multiples valores dentro de una llave hacemos uso de una lista
         adj[int(a[0])] = b
 
     return adj 
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 import helping_functions as hf
+
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import networkx as nx
+
+import sys
+import atexit
+import threading
 
 ## Algoritmos a incluir : 
 ## > Flujo maximo
@@ -53,6 +57,10 @@ def dibujar_grafo(adj):
     canvas = FigureCanvasTkAgg(fig, master=window)
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
+    # Create the first button and add it to the top of the frame
+    arbolMinimo_boton = tk.Button(window, width=35, height=3, text="Flujo máximo")
+    arbolMinimo_boton.pack(side="top", expand=True)
+
 
 
 
@@ -68,7 +76,11 @@ def iniciar_flujo(adj, inicial, final):
     # Creamos el grafp
     dibujar_grafo(adj)
     
-def get_data(s, opcion):
+
+
+
+
+def obtener_grafo(s, opcion):
     # En caso de que no haya ningun dato cuando se pide la matriz, cerramos completamente el programa
     if (len(s) == 0) :
         window.destroy()
@@ -76,9 +88,10 @@ def get_data(s, opcion):
 
     # En caso de que se haya seleccionado el algoritmo para el flujo maximo, se pediran el nodo inicial y el nodo final
     if (opcion == 1) :
-        # Usamos la funcion de ayuda para limpiar la entrada y crear la lista de adyacencia
+        # Usamos la funcion de ayuda para limpiar la entrada y poder hacer uso de ella más fácil
+
         # En este caso se ocupa una tabla hash con multiples valores para cada llave
-        adj = hf.crear_adjList(s)
+        adj = hf.limpiar_entrada(s)
 
         limpiar_pantalla()
 
@@ -86,9 +99,12 @@ def get_data(s, opcion):
         nodoInicial_label = tk.Label(window, text="Nodo inicial :", font='helvetica 20')
         nodoInicial_entry = tk.Entry(window)
 
+        # -> padx(left, right)
+        # -> pady(top, bottom)
+
         # Los agregamos a pantalla
-        nodoInicial_label.pack(pady=(50,0))
-        nodoInicial_entry.pack(padx=(100,100), pady=(0,100))
+        nodoInicial_label.pack(pady=(50,0)) 
+        nodoInicial_entry.pack(padx=(100,100), pady=(10,100))
 
         # Creamos la entiqueta y el campo de entrada para el nodo final
         nodoFinal_label = tk.Label(window, text="Nodo final :", font='helvetica 20')
@@ -96,20 +112,20 @@ def get_data(s, opcion):
 
         # Los agregamos a pantalla
         nodoFinal_label.pack()
-        nodoFinal_entry.pack(padx=(100,100), pady=(0,100))
+        nodoFinal_entry.pack(padx=(100,100), pady=(10,100))
 
         # Creamos el boton para iniciar con el algoritmo
         confirmar_boton = tk.Button(window, width=35, height=3, text="Iniciar", command=lambda:iniciar_flujo(adj, nodoInicial_entry.get(), nodoFinal_entry.get()))
-        confirmar_boton.pack()
+        confirmar_boton.pack(pady = (25,25))
 
         # Creamos un boton para volver al menu principal
         volver_boton = tk.Button(window, width=35, height=3, text="Volver", command=mostrar_menu)
-        volver_boton.pack()
+        volver_boton.pack(pady = (25,25))
 
 def pedir_grafo(opcion):
     limpiar_pantalla()
 
-    # Mostramos instrucciones para ingresar datos
+    # Mostramos instrucciones para ingresar la matriz
     label1 = tk.Label(text = "Ingresar la matriz de adyacencia de manera :", font='helvetica 14')
     label1.pack()
 
@@ -124,15 +140,15 @@ def pedir_grafo(opcion):
     input_grafo.pack()
 
     # Creamos el boton para iniciar con el algoritmo
-    confirmar_boton = tk.Button(window, width=35, height=3, text="OK", command=lambda:get_data(input_grafo.get("1.0",'end-1c'), opcion))
+    confirmar_boton = tk.Button(window, width=35, height=3, text="OK", command=lambda:obtener_grafo(input_grafo.get("1.0",'end-1c'), opcion))
     confirmar_boton.pack(side="top", expand=True)
+
 
     # Creamos un boton para volver al menu principal
     volver_boton = tk.Button(window, width=35, height=3, text="Volver", command=mostrar_menu)
     volver_boton.pack(side="bottom", expand=True)
 
 def mostrar_menu():   
-    opcion = 0
     limpiar_pantalla()
     
     # Create the first button and add it to the top of the frame
@@ -151,7 +167,22 @@ def mostrar_menu():
     button4 = tk.Button(window, width=35, height=3, text="Extra 2")
     button4.pack(side="top", expand=True)
 
+# El siguiente par de funciones son de ayuda para manejar el cierre de la ventana del programa
+# Ya que al usar el graficador de networkx, usa diferentes hilos para su ejecución
+
+def on_closing():
+    sys.exit()
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+def exit_handler():
+    for thread in threading.enumerate():
+        if thread is not threading.main_thread():
+            thread.join()
+atexit.register(exit_handler)
+
 
 mostrar_menu()
-# Run the main loop
+
+
+# Iniciamos el programa
 window.mainloop()
