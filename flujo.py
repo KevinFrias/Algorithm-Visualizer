7adbcdc7dcf3db7e3cbfd62b33c09b2819b34690
Kevin F <knightkevin99@gmail.com>
2023-01-11 20:39:29 -0600

    Ya es posible visualizar el grafo pero su funcion para la modificación todavía presenta varios fallos

---
+++ flujo.py
@@ -0,0 +1,2 @@
+# Para el conseguir la respuesta que se requiere lo que se hace es correr el algoritmo de busqueda en
+# profundidad (DFS) para conseguir todos los caminos de
--- main.py
+++ main.py
@@ -27,17 +27,16 @@ def limpiar_pantalla():
     for widgets in window.winfo_children():
         widgets.destroy()
 
-def dibujar_grafo(adj):
-    limpiar_pantalla()
+# set up the figure and axes
+fig, ax = plt.subplots()
 
+def iniciar_grafo(adj):
     G = nx.Graph()
 
-    # Recorremos la matrix de adyacencia
-
+    # Recorremos la matrix de adyacencia para poder dibujarlo
     for key in adj.keys():
         b = adj[key]
         i = 1
-
         for item in b :
             # Si hay un camino valido lo agregamos
             if item != infinito:
@@ -45,25 +44,79 @@ def dibujar_grafo(adj):
             i += 1
 
 
-    # set up the figure and axes
-    fig, ax = plt.subplots()
+    for n in G.nodes:
+        G.nodes[n]["visible"] = True
 
-    # draw the graph
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, font_weight='bold')
+    for u,v in G.edges: 
+        G[u][v]["visible"] = True
+
+
+    position_graph = nx.spring_layout(G)
+
+
+    nx.draw(G, position_graph, with_labels=True, font_weight='bold')
     labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    nx.draw_networkx_edge_labels(G, position_graph, edge_labels=labels)
 
     # create a FigureCanvasTkAgg widget to display the Matplotlib figure
     canvas = FigureCanvasTkAgg(fig, master=window)
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-    # Create the first button and add it to the top of the frame
-    arbolMinimo_boton = tk.Button(window, width=35, height=3, text="Flujo máximo")
-    arbolMinimo_boton.pack(side="top", expand=True)
+    return G,canvas, position_graph
+
+def modificar_grafo(G, canvas, position_graph):
+
+    ax.clear()  
+    fig.clear()
+
+    #pos = nx.spring_layout(G)
+
+    # Set the node's visibility to False
+    G.nodes[2]["visible"] = False
+    # Set the edges' visibility to False
+    for u, v in G.edges(2):
+        G[u][v]["visible"] = False
+
+    node_list = []
+    edge_list = []
+
+    for i in G.nodes :
+        if G.nodes[i]["visible"] == True :
+            node_list.append(i)
+
+    for u,v in G.edges :
+        if G[u][v]["visible"] == True :
+            edge_list.append((u,v))
 
 
 
+    # Draw the graph, skipping invisible nodes and edges
+    nx.draw(G, position_graph,  with_labels=True, font_weight='bold', nodelist=node_list)
+    nx.draw_networkx_edges(G,position_graph, edgelist=edge_list)
+    labels = nx.get_edge_attributes(G, 'weight')
+
+    for i in labels:
+        for j in i :
+            if (j == 2):
+                del[i]
+                break
+
+    
+
+    print(labels)
+
+    nx.draw_networkx_edge_labels(G,position_graph, edge_labels=labels)
+
+
+    '''
+    G.remove_node(6)
+    nx.draw(G, pos, with_labels=True, font_weight='bold')
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    '''
+
+    canvas.draw()
+    canvas.get_tk_widget().update()
 
 
 def iniciar_flujo(adj, inicial, final):
@@ -72,17 +125,24 @@ def iniciar_flujo(adj, inicial, final):
         window.destroy()
         return ''
 
-    dibujar_grafo(adj)
-    time.sleep(5)
-
+    # Limpiamos la pantalla para poder mostrar toda la información necesaria (El grafo, botones para mostar los pasos del algoritmo y la respuesta)
     limpiar_pantalla()
+    graph,canvas,position_graph = iniciar_grafo(adj)
 
-    # Creamos el grafp
-    dibujar_grafo(adj)
-    
+    # Create the first button and add it to the top of the frame
+    Siguiente_boton = tk.Button(window, width=35, height=3, text="Anterior", command= lambda: modificar_grafo(graph, canvas, position_graph))
+    #Siguiente_boton.place(x=10, y=525)
+    Siguiente_boton.pack(side=tk.LEFT, expand=True)
 
+    # Mostramos instrucciones para ingresar la matriz
+    Resultado = tk.Label(text = "00000", font='helvetica 14')
+    #Resultado.place(x=380, y=545)
+    Resultado.pack(side=tk.LEFT, expand=True)
 
-
+    # Create the first button and add it to the top of the frame
+    Anterior_boton = tk.Button(window, width=35, height=3, text="Siguiente")
+    #Anterior_boton.place(x=480, y=525)
+    Anterior_boton.pack(side=tk.LEFT, expand=True)
 
 def obtener_grafo(s, opcion):
     # En caso de que no haya ningun dato cuando se pide la matriz, cerramos completamente el programa
@@ -92,11 +152,11 @@ def obtener_grafo(s, opcion):
 
     # En caso de que se haya seleccionado el algoritmo para el flujo maximo, se pediran el nodo inicial y el nodo final
     if (opcion == 1) :
-        # Usamos la funcion de ayuda para limpiar la entrada y poder hacer uso de ella más fácil
-
-        # En este caso se ocupa una tabla hash con multiples valores para cada llave
+        # Limpiamos la entrada que fue proporcionada por el usuario, esto con una funcion de ayuda en otro archivo, para
+        # su posterior asignacion a una variable
         adj = hf.limpiar_entrada(s)
 
+        # Limpiamos la pnatalla para poder pedir los datos restantes para el algoritmo
         limpiar_pantalla()
 
         # Creamos la entiqueta y el campo de entrada para el nodo inicial
@@ -110,6 +170,7 @@ def obtener_grafo(s, opcion):
         nodoInicial_label.pack(pady=(50,0)) 
         nodoInicial_entry.pack(padx=(100,100), pady=(10,50))
 
+
         # Creamos la entiqueta y el campo de entrada para el nodo final
         nodoFinal_label = tk.Label(window, text="Nodo final :", font='helvetica 20')
         nodoFinal_entry = tk.Entry(window, font=('Georgia 20'))
@@ -118,9 +179,10 @@ def obtener_grafo(s, opcion):
         nodoFinal_label.pack()
         nodoFinal_entry.pack(padx=(100,100), pady=(10,50))
 
-        # Creamos el boton para iniciar con el algoritmo
+        # Creamos el boton para iniciar con el algoritmo, el cual mandara a llamar una funcion con los parametros de entrada del usuario
         confirmar_boton = tk.Button(window, width=35, height=3, text="Iniciar", command=lambda:iniciar_flujo(adj, nodoInicial_entry.get(), nodoFinal_entry.get()))
         confirmar_boton.pack(pady = (25,25))
+
 
         # Creamos un boton para volver al menu principal
         volver_boton = tk.Button(window, width=35, height=3, text="Volver", command=mostrar_menu)
@@ -171,6 +233,7 @@ def mostrar_menu():
     button4 = tk.Button(window, width=35, height=3, text="Extra 2")
     button4.pack(side="top", expand=True)
 
+
 # El siguiente par de funciones son de ayuda para manejar el cierre de la ventana del programa
 # Ya que al usar el graficador de networkx, usa diferentes hilos para su ejecución
 
@@ -186,7 +249,6 @@ atexit.register(exit_handler)
 
 
 mostrar_menu()
-
 
 # Iniciamos el programa
 window.mainloop()
